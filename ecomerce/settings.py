5dafbf5cc2567e4688930387c2c926d2d3d157af
Gedihared6409 <gediali019@gmail.com>
2023-09-13 23:01:07 +0900

    added configuration to the wsgi

--- ecomerce/settings.py
+++ ecomerce/settings.py
@@ -103,7 +103,7 @@ else:
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-ALLOWED_HOSTS = ['ecomercce.herokuapp.com','127.0.0.1','ecomerce.up.railway.app']
+ALLOWED_HOSTS = ['.vercel.app']
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
